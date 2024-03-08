048309206a974b2a89436eed07de1ab6710bc56c
mnbarber <mnbarber1993@gmail.com>
2024-03-08 10:18:04 -0800

    adding currencies nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Home from "./Home/Home";
+import Currencies from "./Currencies/Currencies";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [price, setPrice] = useState(null);
@@ -14,9 +16,13 @@ function App() {
       <nav>
         <img src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png" alt="" />
         <h1>Bitcoin prices</h1>
+        <a href='/currencies'>Currencies</a>
       </nav>
       <main>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/currencies" element={<Currencies />} />
+        </Routes>
       </main>
     </div>
   );
